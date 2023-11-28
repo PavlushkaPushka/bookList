5fc44a0d89959f3930e95a13d81d693eceb829e8
Pavel <63355165+PavlushkaPushka@users.noreply.github.com>
2023-11-28 05:43:40 +0000

    for start project

--- list_book/src/items/modalForDelete.js
+++ list_book/src/items/modalForDelete.js
@@ -5,16 +5,17 @@ function ModalForDelete({ setActive, book }) {
     const { removeBook } = useContext(Context)
 
     return (
+        <div className="form__shell">
+            <div class="form">
+                <div class="title">Do you really want delete this book?</div>
+                <div className="submit__container">
+                    <button type="submit" class="submit add" onClick={() => { removeBook(book.id); setActive() }}>Yes</button>
+                    <button type="reset" class="submit cancel" onClick={() => setActive()}>No</button>
+                </div>
+            </div >
+        </div>
 
-        <div class="form">
-            <div class="title">Do you really want delete this book?</div>
-            <div className="submit__container">
 
-                <button type="submit" class="submit add" onClick={() => { removeBook(book.id); setActive() }}>Yes</button>
-                <button type="reset" class="submit cancel" onClick={() => setActive()}>No</button>
-            </div>
-
-        </div >
     )
 }
 
