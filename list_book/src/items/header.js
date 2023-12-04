c262f990b2e4f941dd4313f3948fe9fa5c2e5156
PvlshkaPushka <parabellum.gmm@gmail.com>
2023-12-04 21:33:26 +0500

    add a normal button for header without functional

--- list_book/src/items/header.js
+++ list_book/src/items/header.js
@@ -6,10 +6,11 @@ function Info({ setActive, modalInside, setBook, books }) {
     return (
         <div className="header">
             <div className="info">
-                <p>On this page you can add some book</p>
+                <p>On this page you can</p>
             </div>
             <div className="button">
-                <ButtonCreate setActive={setActive} modalInside={modalInside} setBook={setBook} books={books} />
+                <button className="submit addBook" type="button">add a new book</button>
+                {/* <ButtonCreate setActive={setActive} modalInside={modalInside} setBook={setBook} books={books} /> */}
             </div>
 
         </div >
