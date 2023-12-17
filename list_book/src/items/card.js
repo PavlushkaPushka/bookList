1213b7565041c4eddab46e9eb89455db31f18761
PvlshkaPushka <parabellum.gmm@gmail.com>
2023-12-17 20:47:25 +0500

    change on desighn and verstka

--- list_book/src/items/card.js
+++ list_book/src/items/card.js
@@ -1,4 +1,5 @@
 import "./../css/card.css"
+import ModalForDelete from "./modalForDelete";
 import ButtonDelete from "./buttonDelete";
 
 function Card({ aboutBook, setActive, modalInside }) {
@@ -11,7 +12,15 @@ function Card({ aboutBook, setActive, modalInside }) {
                     <div className="content">
                         <h3>{aboutBook.bookName}</h3>
                         <h2>{aboutBook.author}</h2>
-                        <ButtonDelete setActive={setActive} modalInside={modalInside} book={aboutBook} />
+                        <div className="buttons_container">
+                            <button className="submit change_card" type="button">Change</button>
+                            {/* <button className="submit delete_card" type="button" onClick={() => {
+                                setActive(true); //////////////// остановился здесь!!!
+                                modalInside(() => (<ModalForDelete setActive={setActive} book={book} />))
+                            }}>Delete</button> */}
+                        </div>
+
+                        {/* <ButtonDelete setActive={setActive} modalInside={modalInside} book={aboutBook} /> */}
                     </div>
                 </div>
             </div>
