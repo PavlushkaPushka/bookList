dd971d359d7f30cbc90cae5c62e7bbf4be2cfa43
Pavel <parabellum.gmm@gmail.com>
2024-01-09 15:38:34 +0500

    some change on card

--- list_book/src/items/card.js
+++ list_book/src/items/card.js
@@ -2,65 +2,63 @@ import "./../css/card.css"
 import ModalForDelete from "./modalForDelete";
 import { useState } from "react"
 
-function ButtonChangeActive ({ aboutBook, setActive, modalInside }) {
-    console.log(aboutBook)
-    return (
-        <div className="content">
-            <h3>{aboutBook.bookName}</h3>
-            <h2>{aboutBook.author}</h2>
-                <div className="buttons_container">
-                    <button className="submit change_card" type="button">just check this shiit</button>
-                    <button className="submit delete_card" type="button" onClick={() => {
-                        setActive(true);
-                        modalInside(() => (<ModalForDelete setActive={setActive} book={aboutBook} />))
-                    }}>Delete</button>
-            </div>
-    </div>
-    );
-}
-
-function ButtonChangeNotActive ({ aboutBook, setActive, modalInside }) {
-    return (
-        <div className="content">
-            <h3>{aboutBook.bookName}</h3>
-            <h2>{aboutBook.author}</h2>
-                <div className="buttons_container">
-                    <button className="submit change_card" type="button">Change</button>
-                    <button className="submit delete_card" type="button" onClick={() => {
-                        setActive(true);
-                        modalInside(() => (<ModalForDelete setActive={setActive} book={aboutBook} />))
-                    }}>Delete</button>
-                </div>
-        </div>
-    );
-}
 
 function Card({ aboutBook, setActive, modalInside }) {
     
     const [buttonChange, setButtonChange] = useState(false)
-
+    // console.log(aboutBook)
     return (
 
         <div className="container">
             <div className="card">
                 <div className="box">
-                    {buttonChange ? <ButtonChangeNotActive aboutBook={aboutBook} setActive={setActive} modalInside={modalInside}/> : <ButtonChangeActive aboutbook={aboutBook} setActive={setActive} modalInside={modalInside}/>}
-                    {/* <div className="content">
-                        <h3>{aboutBook.bookName}</h3>
-                        <h2>{aboutBook.author}</h2>
-                        <div className="buttons_container">
-                            <button className="submit change_card" type="button">Change</button>
-                            <button className="submit delete_card" type="button" onClick={() => {
-                                setActive(true);
-                                modalInside(() => (<ModalForDelete setActive={setActive} book={aboutBook} />))
-                            }}>Delete</button>
-                        </div>
-                    </div> */}
+                    {buttonChange ? <ButtonChangeNotActive aboutBook={aboutBook} setActive={setActive} modalInside={modalInside}/> : <ButtonChangeActive aboutBook={aboutBook} setActive={setActive} modalInside={modalInside}/>}
                 </div>
             </div>
         </div>
 
     );
+
+
+
+    function ButtonChangeActive ({ aboutBook, setActive, modalInside }) {
+
+        return (
+            <div className="content">
+                <h3>{aboutBook.bookName}</h3>
+                <h2>{aboutBook.author}</h2>
+                    <div className="buttons_container">
+                        <button className="submit change_card" type="button" onClick={() => {
+                            setButtonChange(true);
+                        }}>change</button>
+                        <button className="submit delete_card" type="button" onClick={() => {
+                            setActive(true);
+                            modalInside(() => (<ModalForDelete setActive={setActive} book={aboutBook} />))
+                        }}>Delete</button>
+                </div>
+        </div>
+        );
+    }
+    
+    function ButtonChangeNotActive ({ aboutBook, setActive, modalInside }) {
+        return (
+            <div className="content">
+                <h3>{aboutBook.bookName}</h3>
+                <h2>{aboutBook.author}</h2>
+                    <div className="buttons_container">
+                    <button className="submit change_card" type="button" onClick={() => {
+                            setButtonChange(false);
+                        }}>just check this shit</button>
+                        <button className="submit delete_card" type="button" onClick={() => {
+                            setActive(true);
+                            modalInside(() => (<ModalForDelete setActive={setActive} book={aboutBook} />))
+                        }}>Delete</button>
+                    </div>
+            </div>
+        );
+    }
 }
+
+
 
 export default Card;
